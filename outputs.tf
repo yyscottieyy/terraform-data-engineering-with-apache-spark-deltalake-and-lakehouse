4c142ea54cad265e6a2aaddce7a82e14ceb1c7b2
yyscottieyy <yyscottie@gmail.com>
2023-04-25 23:00:44 -0700

    Commenting out vnet variable

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "resource_group_id" {
     value = azurerm_resource_group.rg.id
 }
 
-output "virtual_network_id" {
+/* output "virtual_network_id" {
     value = azurerm_virtual_network.vnet.id
-}
+} */
