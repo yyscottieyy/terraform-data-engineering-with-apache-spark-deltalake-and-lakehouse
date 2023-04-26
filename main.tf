148efee887597ef32e7d52756f9bb782080a1c51
yyscottieyy <yyscottie@gmail.com>
2023-04-25 22:22:05 -0700

    Use East US instead of West US

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
-  location = "westus2"
+  location = var.region
 
   tags = {
     Environment = "Development"
@@ -37,7 +37,7 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_virtual_network" "vnet" {
   name                = var.virtual_network_name
   address_space       = ["10.0.0.0/16"]
-  location            = var.virtual_network_location
+  location            = var.region
   resource_group_name = azurerm_resource_group.rg.name
 }
 
@@ -45,7 +45,7 @@ resource "azurerm_virtual_network" "vnet" {
 resource "azurerm_mssql_server" "sqlserver" {
   name                         = "salesdb-server-ade"
   resource_group_name          = azurerm_resource_group.rg.name
-  location                     = var.virtual_network_location
+  location                     = var.region
   version                      = "12.0"
   administrator_login          = "training_admin"
   administrator_login_password = "scotT123$$"
@@ -60,6 +60,7 @@ resource "azurerm_mssql_server" "sqlserver" {
 # Create a Azure SQL DB database
 resource "azurerm_mssql_database" "sqldb" {
   name                         = "salesdb-ade"
+  location                     = var.region
   server_id                    = azurerm_mssql_server.sqlserver.id
   collation                    = "SQL_Latin1_General_CP1_CI_AS"
   license_type                 = "LicenseIncluded"
